6c786f4e4548b35b731d48d8b68627b532ab2324
andrey shalagin <andreyshalagin@gmail.com>
2025-11-21 16:39:39 +0300

    Пример объявления функции. Тесты на функцию

---
+++ src/main/kotlin/language_bases/Functions.kt
@@ -0,0 +1,5 @@
+package language_bases
+
+fun max(a: Int, b: Int): Int {
+    return if (a < b) b else a
+}
---
+++ src/test/kotlin/language_bases/FunctionsTest.kt
@@ -0,0 +1,34 @@
+package language_bases
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class FunctionsTest {
+
+
+    @Test
+    fun max() {
+        assertEquals(7, max(5, 7))
+    }
+
+    @Test
+    fun maxWithZero() {
+        assertEquals(5, max(5, 0))
+    }
+
+    @Test
+    fun maxWithNegativeAndPositive() {
+        assertEquals(3, max(-5, 3))
+    }
+
+    @Test
+    fun maxWithNegativesOnly() {
+        assertEquals(-5, max(-5, -25))
+    }
+
+    @Test
+    fun maxWithTheSame() {
+        assertEquals(5, max(5, 5))
+    }
+}
