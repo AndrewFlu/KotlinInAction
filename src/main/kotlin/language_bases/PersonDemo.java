c7e414433a60515a250480acce431b5a71b7e4b9
andrey shalagin <andreyshalagin@gmail.com>
2025-11-23 22:31:30 +0300

    вызов kotlin-класса из java-кода

---
+++ src/main/kotlin/language_bases/PersonDemo.java
@@ -0,0 +1,11 @@
+package language_bases;
+
+public class PersonDemo {
+    public static void main(String[] args) {
+        Person robert = new Person("Robert", true);
+        System.out.println("name: " + robert.getName());
+        System.out.println("is student?: " + robert.isStudent());
+        robert.setStudent(false); // выпускной
+        System.out.println("is student?: " + robert.isStudent());
+    }
+}
