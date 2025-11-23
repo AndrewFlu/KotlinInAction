e7dc7cd426a44bc3ba7a4efb88226dd4272ef3d0
andrey shalagin <andreyshalagin@gmail.com>
2025-11-23 22:51:37 +0300

    Пример вычисляемого свойства класса

---
+++ src/main/kotlin/language_bases/Rectangle.kt
@@ -0,0 +1,8 @@
+package language_bases
+
+class Rectangle (val width: Int, val height: Int) {
+    val isSquare: Boolean
+        get() {
+            return width == height
+        }
+}
---
+++ src/main/kotlin/language_bases/RectangleDemo.kt
@@ -0,0 +1,8 @@
+package language_bases
+
+fun main() {
+    val a = Rectangle(15, 17)
+    println(a.isSquare)
+    val b = Rectangle(30, 30)
+    println(b.isSquare)
+}
