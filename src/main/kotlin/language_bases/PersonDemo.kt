4d7407d9d5f72b5f86f57fb591155100bc88909a
andrey shalagin <andreyshalagin@gmail.com>
2025-11-23 22:40:51 +0300

    вызов kotlin-класса из kotlin-кода

---
+++ src/main/kotlin/language_bases/PersonDemo.kt
@@ -0,0 +1,9 @@
+package language_bases
+
+fun main() {
+    val mike = Person("Mike", true);
+    println("name: ${mike.name}")
+    println("is student?: ${mike.isStudent}")
+    mike.isStudent = false
+    println("is student?: ${mike.isStudent}")
+}
