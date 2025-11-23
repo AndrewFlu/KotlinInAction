1840a5a42e7561e4fb19216e59307cdd3ac73d97
andrey shalagin <andreyshalagin@gmail.com>
2025-11-23 22:26:32 +0300

    java-класс Person converted to kotlin file (class)

--- src/main/kotlin/language_bases/Person.kt
+++ src/main/kotlin/language_bases/Person.kt
@@ -1,13 +1,3 @@
-package language_bases;
+package language_bases
 
-public class Person {
-    private final String name;
-
-    public Person(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
-}
+class Person(val name: String)
