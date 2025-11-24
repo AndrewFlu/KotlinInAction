b8adb08d81f1ce5d47119d66e58508bf7e8cb843
andrey shalagin <andreyshalagin@gmail.com>
2025-11-24 10:45:20 +0300

    использование when на примере мнемоник цветового спектра

---
+++ src/main/kotlin/ch02/colors/Mnemonics.kt
@@ -0,0 +1,17 @@
+package ch02.colors
+
+fun getMnemonic(color: Color) =
+    when (color) {
+        Color.RED -> "Каждый"
+        Color.ORANGE -> "Охотник"
+        Color.YELLOW -> "Желает"
+        Color.GREEN -> "Знать"
+        Color.BLUE -> "Где"
+        Color.INDIGO -> "Сидит"
+        Color.VIOLET -> "Фазан"
+    }
+
+fun main() {
+    val color = Color.VIOLET
+    println("Mnemonic for $color is [${getMnemonic(color)}]")
+}
