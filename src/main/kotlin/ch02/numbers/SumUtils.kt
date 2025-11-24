f86808eb9e047b28609bb94739d6ed16c0cdfe50
andrey shalagin <andreyshalagin@gmail.com>
2025-11-24 16:15:28 +0300

    refactor: заменена внутренняя реализация функции. Вместо выражения if используется выражение when

--- src/main/kotlin/ch02/numbers/SumUtils.kt
+++ src/main/kotlin/ch02/numbers/SumUtils.kt
@@ -1,12 +1,11 @@
 package ch02.numbers
 
 fun evaluate(e: Expression): Int =
-    if (e is Number)
-        e.value
-    else if (e is Summarized)
-        evaluate(e.right) + evaluate(e.left) // "умное" преобразование
-    else
-        throw IllegalArgumentException("Unknown Expression")
+    when (e) {
+        is Number -> e.value
+        is Summarized -> evaluate(e.left) + evaluate(e.right)
+        else -> throw IllegalArgumentException("Unknown Expression")
+    }
 
 fun main() {
     println(evaluate(Summarized(Summarized(Number(1), Number(4)), Number(5))))
