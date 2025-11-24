a58eae6e453253d213311bdd074c00afdf38cd2c
andrey shalagin <andreyshalagin@gmail.com>
2025-11-24 16:25:39 +0300

    пример использования блоков в ветках конструкций if и when

--- src/main/kotlin/ch02/numbers/SumUtils.kt
+++ src/main/kotlin/ch02/numbers/SumUtils.kt
@@ -7,6 +7,24 @@ fun evaluate(e: Expression): Int =
         else -> throw IllegalArgumentException("Unknown Expression")
     }
 
+fun evaluateWithLogging(e: Expression): Int =
+    when (e) {
+        is Number -> {
+            println("Number: ${e.value}")
+            e.value
+        }
+
+        is Summarized -> {
+            val leftResult = evaluateWithLogging(e.left)
+            val rightResult = evaluateWithLogging(e.right)
+            println("sum: $leftResult + $rightResult")
+            leftResult + rightResult
+        }
+
+        else -> throw IllegalArgumentException("Unknown Expression")
+    }
+
 fun main() {
-    println(evaluate(Summarized(Summarized(Number(1), Number(4)), Number(5))))
+//    println(evaluate(Summarized(Summarized(Number(1), Number(4)), Number(5))))
+    println(evaluateWithLogging(Summarized((Summarized(Number(5), Number(10))), Number(10))))
 }
