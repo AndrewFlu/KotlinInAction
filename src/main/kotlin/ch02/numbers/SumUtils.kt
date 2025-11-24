0171d4c5ae19f780f01e50fe4d978fb042618be6
andrey shalagin <andreyshalagin@gmail.com>
2025-11-24 16:12:35 +0300

    refactor: удалены фигурные скобки

--- src/main/kotlin/ch02/numbers/SumUtils.kt
+++ src/main/kotlin/ch02/numbers/SumUtils.kt
@@ -1,13 +1,12 @@
 package ch02.numbers
 
 fun evaluate(e: Expression): Int =
-    if (e is Number) {
+    if (e is Number)
         e.value
-    } else if (e is Summarized) {
+    else if (e is Summarized)
         evaluate(e.right) + evaluate(e.left) // "умное" преобразование
-    } else {
+    else
         throw IllegalArgumentException("Unknown Expression")
-    }
 
 fun main() {
     println(evaluate(Summarized(Summarized(Number(1), Number(4)), Number(5))))
