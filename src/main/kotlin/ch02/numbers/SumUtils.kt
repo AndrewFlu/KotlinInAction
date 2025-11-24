d722956e6d6059e834cb70134ae3134cdcfb90ce
andrey shalagin <andreyshalagin@gmail.com>
2025-11-24 16:11:31 +0300

    refactor: преобразование тела функции в выражение на основе if

--- src/main/kotlin/ch02/numbers/SumUtils.kt
+++ src/main/kotlin/ch02/numbers/SumUtils.kt
@@ -1,15 +1,13 @@
 package ch02.numbers
 
-fun evaluate(e: Expression) : Int {
+fun evaluate(e: Expression): Int =
     if (e is Number) {
-        val n = e as Number
-        return n.value
+        e.value
+    } else if (e is Summarized) {
+        evaluate(e.right) + evaluate(e.left) // "умное" преобразование
+    } else {
+        throw IllegalArgumentException("Unknown Expression")
     }
-    if (e is Summarized) {
-        return evaluate(e.right) + evaluate(e.left) // "умное" преобразование
-    }
-    throw IllegalArgumentException("Unknown Expression")
-}
 
 fun main() {
     println(evaluate(Summarized(Summarized(Number(1), Number(4)), Number(5))))
