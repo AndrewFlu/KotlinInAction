4d8909222f3516385e1acff2b730152554d7e3d2
andrey shalagin <andreyshalagin@gmail.com>
2025-11-24 10:59:07 +0300

    refactor: пример захвата переменной в области действия when

--- src/main/kotlin/ch02/colors/GroupingColorByTemperature.kt
+++ src/main/kotlin/ch02/colors/GroupingColorByTemperature.kt
@@ -5,8 +5,7 @@ import ch02.colors.Color.*
 fun measureColor() = GREEN;
 
 fun getWarmthFromSensor(): String {
-    val color = measureColor()
-    return when (color) {
+    return when (val color = measureColor()) {
         RED, ORANGE, YELLOW -> "warm (red = ${color.r})"
         GREEN -> "neutral (green = ${color.g})"
         BLUE, INDIGO, VIOLET -> "cold (blue = ${color.b})"
