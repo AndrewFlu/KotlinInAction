c247ed1553cf53b59a0025c72ec675fef6539d54
andrey shalagin <andreyshalagin@gmail.com>
2025-11-25 09:45:26 +0300

    Добавлена нисходящая обработка диапазона в цикле for(<число> in <от downTo до> step 2), где 'step 2' — шаг по диапазону

--- src/main/kotlin/ch02/loops/FizzBuzzGame.kt
+++ src/main/kotlin/ch02/loops/FizzBuzzGame.kt
@@ -4,12 +4,18 @@ fun fizzBuzz(i: Int) =
     when {
         i % 15 == 0 -> "fizz-buzz"
         i % 3 == 0 -> "fizz"
-        i % 5 ==0 -> "buzz"
+        i % 5 == 0 -> "buzz"
         else -> "$i"
     }
 
 fun main() {
-    for (i in 1..100) {
+    println("New Game (count up)")
+    for (i in 1..30) {
+        println(fizzBuzz(i))
+    }
+
+    println("New Game (count down)")
+    for (i in 50 downTo 1 step 2) {
         println(fizzBuzz(i))
     }
 }
