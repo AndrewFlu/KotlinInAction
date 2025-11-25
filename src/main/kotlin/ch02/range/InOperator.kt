be423cd185ec7c35a9aeeb3d0479e304d33b34d2
andrey shalagin <andreyshalagin@gmail.com>
2025-11-25 14:17:53 +0300

    операторы принадлежности к диапазону: in и !in

---
+++ src/main/kotlin/ch02/range/InOperator.kt
@@ -0,0 +1,10 @@
+package ch02.range
+
+fun isLetter(c: Char) = c in 'a'..'z' || c in 'A'..'Z'
+fun isNotDigit(c: Char) = c !in '1'..'9'
+
+fun main() {
+    println(isLetter('y'))
+    println(isNotDigit('7'))
+    println(isNotDigit('q'))
+}
