f2c157d340fb198c917ee3ddbc2d210b388c02e2
andrey shalagin <andreyshalagin@gmail.com>
2025-11-25 14:56:50 +0300

    Работа с исключениями

---
+++ src/main/kotlin/ch02/exceptions/PercentValidator.kt
@@ -0,0 +1,14 @@
+package ch02.exceptions
+
+fun validatePercent(number: Int) =
+    if (number in 0..100)
+        true
+else
+    throw IllegalArgumentException(
+        "A percentage value must be between 0 and 100: $number"
+    )
+
+fun main(){
+    val number = 111
+    println("$number is a valid percent: ${validatePercent(number)}")
+}
