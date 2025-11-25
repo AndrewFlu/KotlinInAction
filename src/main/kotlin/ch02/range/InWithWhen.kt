6ebbbbe57f995aa65707dbefc77d514399f528a8
andrey shalagin <andreyshalagin@gmail.com>
2025-11-25 14:41:28 +0300

    использование оператора in в выражении when. Наблюдение: в диапазон кириллических символов 'а'..'я' НЕ ВХОДИТ 'ё', но ВХОДИТ 'й'

---
+++ src/main/kotlin/ch02/range/InWithWhen.kt
@@ -0,0 +1,17 @@
+package ch02.range
+
+fun recognize(c: Char) = when(c) {
+    in '0'..'9' -> "It's a digit!"
+    in 'a'..'z', in 'A'..'Z',
+    in 'а'..'я', in 'А'..'Я'
+        ,'ё' -> "It's a letter!"
+    else -> "Hm.., it's a Stange char..."
+}
+
+fun main() {
+    println(recognize('a'))
+    println(recognize('5'))
+    println(recognize('й'))
+    println(recognize('ё'))
+    println(recognize('е'))
+}
