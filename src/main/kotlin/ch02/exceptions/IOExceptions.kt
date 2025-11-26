38f64c5516076d6f3e45280d10616eab8cbc7ab3
andrey shalagin <andreyshalagin@gmail.com>
2025-11-26 09:25:56 +0300

    использование блока try — в качестве выражения

--- src/main/kotlin/ch02/exceptions/IOExceptions.kt
+++ src/main/kotlin/ch02/exceptions/IOExceptions.kt
@@ -21,9 +21,22 @@ fun readNumber2(reader: BufferedReader): Int {
     return Integer.parseInt(line)
 }
 
+fun readNumber3(reader: BufferedReader) {
+    val number = try {
+        Integer.parseInt(reader.readLine())
+    } catch (e: NumberFormatException) {
+        return
+    }
+
+    println(number)
+}
+
+
 fun main() {
     val reader = BufferedReader(StringReader("12345"))
     val reader2 = BufferedReader(StringReader("notANumber"))
+    val reader3 = BufferedReader(StringReader("5"))
     println(readNumber(reader))
+    println(readNumber3(reader3))
     println(readNumber2(reader2))
 }
