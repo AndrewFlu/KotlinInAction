800ed67ee856696374c39dff5ec87524ec2239e2
andrey shalagin <andreyshalagin@gmail.com>
2025-11-28 13:58:10 +0300

    Пример вызова функции-расширения из кода Java

---
+++ src/main/kotlin/ch03_functions/inheritance/InheritanceTestDrive.java
@@ -0,0 +1,8 @@
+package ch03_functions.inheritance;
+
+public class InheritanceTestDrive {
+    public static void main(String[] args) {
+        Element button = new Button();
+        ExtensionsKt.clear(button);
+    }
+}
