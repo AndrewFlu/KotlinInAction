7d9ff00c88b4fd7593a95991547ed176ce84ae1e
andrey shalagin <andreyshalagin@gmail.com>
2025-11-28 09:45:45 +0300

    refactor: глобавльная функция превратилась в функцию-расширение интерфейса Collection

--- src/main/kotlin/ch03_functions/strings/join.kt
+++ src/main/kotlin/ch03_functions/strings/join.kt
@@ -2,14 +2,13 @@
 
 package ch03_functions.strings
 
-fun <T> joinToString(
-    collection: Collection<T>,
-    separator: String,
-    prefix: String,
-    postfix: String
+fun <T> Collection<T>.joinToString(
+    separator: String = ", ",
+    prefix: String = "",
+    postfix: String = ""
 ): String {
     val result = StringBuilder(prefix)
-    for ((index, element) in collection.withIndex()) {
+    for ((index, element) in this.withIndex()) {
         if (index > 0)
             result.append(separator) // перед первым элементом разделитель не добавляется
         result.append(element)
