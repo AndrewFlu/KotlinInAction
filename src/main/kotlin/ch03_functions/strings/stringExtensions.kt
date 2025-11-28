eb57b0784c0dd8e09ea9eec2a197a813f0bd9737
andrey shalagin <andreyshalagin@gmail.com>
2025-11-28 18:01:25 +0300

    Изменяемое свойство-расширение

--- src/main/kotlin/ch03_functions/strings/stringExtensions.kt
+++ src/main/kotlin/ch03_functions/strings/stringExtensions.kt
@@ -1,14 +1,25 @@
 package ch03_functions.strings
 
 // функция-расширение
-fun String.lastChar() : Char = get(length - 1)
+fun String.lastChar(): Char = get(length - 1)
 
 // свойство-расширение
 val String.lastChar: Char
     get() = get(length - 1)
 
+var StringBuilder.tailChar: Char
+    get() = this.get(length - 1)
+    set(value) {
+        this.setCharAt(length - 1, value)
+    }
+
 
 fun main() {
     println("MacBook".lastChar())
     println("Kotlin".lastChar)
+
+    val sb: StringBuilder = StringBuilder("Kotlin?")
+    println(sb.tailChar)
+    sb.tailChar = '!'
+    println(sb)
 }
