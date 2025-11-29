6e2bf70904d0b0e259ebd714da243bab9a4ebac8
andrey shalagin <andreyshalagin@gmail.com>
2025-11-29 10:43:06 +0300

    Т.к. у локальной функции есть доступ с полям внешней функции - можно избавиться от параметра User

--- src/main/kotlin/ch03_functions/local_functions/User.kt
+++ src/main/kotlin/ch03_functions/local_functions/User.kt
@@ -5,14 +5,14 @@ class User(val id: Int, val name: String, val address: String)
 fun saveUser(user: User) {
 
     // объявление локальной функции для валидации полей
-    fun validate(user: User, value: String, fieldName: String) {
+    fun validate(value: String, fieldName: String) {
         if (value.isEmpty())
             throw IllegalArgumentException(
                 "Can't save user ${user.id}: empty $fieldName")
     }
     // вызов локальной функции для валидация опеределённых полей
-    validate(user, user.name, "Name")
-    validate(user, user.address, "Address")
+    validate(user.name, "Name")
+    validate(user.address, "Address")
 }
 
 // сохранение данных пользователя в базе данных
