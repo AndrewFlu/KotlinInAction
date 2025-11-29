f139131bbcb3a0e51be538b57968e06aefcc9248
andrey shalagin <andreyshalagin@gmail.com>
2025-11-29 10:40:22 +0300

    избавление от дублирования через объявление и использование локальной функции (валидации полей)

--- src/main/kotlin/ch03_functions/local_functions/User.kt
+++ src/main/kotlin/ch03_functions/local_functions/User.kt
@@ -3,18 +3,19 @@ package ch03_functions.local_functions
 class User(val id: Int, val name: String, val address: String)
 
 fun saveUser(user: User) {
-    // валидация полей продублирована
-    if (user.name.isEmpty())
-        throw IllegalArgumentException(
-            "Can't save user ${user.id}: empty name"
-        )
-    if (user.address.isEmpty())
-        throw IllegalArgumentException(
-            "Can't save user ${user.id}: empty address"
-        )
+
+    // объявление локальной функции для валидации полей
+    fun validate(user: User, value: String, fieldName: String) {
+        if (value.isEmpty())
+            throw IllegalArgumentException(
+                "Can't save user ${user.id}: empty $fieldName")
+    }
+    // вызов локальной функции для валидация опеределённых полей
+    validate(user, user.name, "Name")
+    validate(user, user.address, "Address")
 }
 
 // сохранение данных пользователя в базе данных
 fun main() {
-    saveUser(User(1, "", ""))
+    saveUser(User(2, "", ""))
 }
