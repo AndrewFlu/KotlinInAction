7c31192cadff84d37ec19385131c2ac36d806840
andrey shalagin <andreyshalagin@gmail.com>
2025-12-01 11:15:54 +0300

    Добавление нового класса в иерархию Expr. Основные тесты для функции eval(e: Expr)

---
+++ src/test/kotlin/ch04_classes_interfaces/sealed_classes/sealed/HierarchyBySealedClassesKtTest.kt
@@ -0,0 +1,29 @@
+package ch04_classes_interfaces.sealed_classes.sealed
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class HierarchyBySealedClassesKtTest {
+
+    @Test
+    fun evalSum() {
+        val sumExpr = Sum(4, 6)
+
+        assertEquals(10, eval(sumExpr))
+    }
+
+    @Test
+    fun evalMultiply() {
+        val mulExpr = Mul(5, 5)
+
+        assertEquals(25, eval(mulExpr))
+    }
+
+    @Test
+    fun evalNumber() {
+        val numExpr = Num(7)
+
+        assertEquals(7, eval(numExpr))
+    }
+}
