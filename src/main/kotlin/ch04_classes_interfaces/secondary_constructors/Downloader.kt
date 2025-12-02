561e30c36eb8935704892eb55e4b03a322216b66
andrey shalagin <andreyshalagin@gmail.com>
2025-12-02 20:34:41 +0300

    Примеры создания вторичных конструкторов на Kotlin (и Java)

---
+++ src/main/kotlin/ch04_classes_interfaces/secondary_constructors/Downloader.kt
@@ -0,0 +1,14 @@
+package ch04_classes_interfaces.secondary_constructors
+
+import java.net.URI
+
+class Downloader {
+
+    constructor(url: String?) {
+        // код
+    }
+
+    constructor(uri: URI?) {
+        // код
+    }
+}
---
+++ src/main/kotlin/ch04_classes_interfaces/secondary_constructors/JDownloader.java
@@ -0,0 +1,14 @@
+package ch04_classes_interfaces.secondary_constructors;
+
+import java.net.URI;
+
+public class JDownloader {
+
+    public JDownloader(String url) {
+        // код
+    }
+
+    public JDownloader(URI uri) {
+        // код
+    }
+}
