b5f88b32e8c167b5a6a0d4d9a8fcbd972d42fa12
andrey shalagin <andreyshalagin@gmail.com>
2025-12-03 21:07:29 +0300

    Переопределение метода equals() для логического сравнения на равенство объектов с помощью "=="

--- src/main/kotlin/ch04_classes_interfaces/override_methods/Customer.kt
+++ src/main/kotlin/ch04_classes_interfaces/override_methods/Customer.kt
@@ -5,9 +5,17 @@ class Customer(val name: String, val postalCode: String) {
     override fun toString(): String {
         return "Customer(name=$name, postalCode=$postalCode)"
     }
+
+    override fun equals(other: Any?): Boolean {
+        if (other == null || other !is Customer)
+            return false
+        return name == other.name && postalCode == other.postalCode
+    }
 }
 
 fun main() {
-    val zhenya = Customer("Zhenya", "Yo, Uraeva 6b")
-    println(zhenya)
+    val zhenya1 = Customer("Zhenya", "Yo, Uraeva 6b")
+    val zhenya2 = Customer("Zhenya", "Yo, Uraeva 6b")
+
+    println(zhenya1 == zhenya2)
 }
