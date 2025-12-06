a51ecb0711abfdf070f9e710a0925d60acf4ffd8
andrey shalagin <andreyshalagin@gmail.com>
2025-12-06 11:44:14 +0300

    Расширение интерфейса встроенным классом

--- src/main/kotlin/ch04_classes_interfaces/embeded_classes/BudgetSystem.kt
+++ src/main/kotlin/ch04_classes_interfaces/embeded_classes/BudgetSystem.kt
@@ -1,12 +1,27 @@
 package ch04_classes_interfaces.embeded_classes
 
+import kotlin.math.exp
+
 fun addExpecnce(axpence: RusRubAmount) {
     // сохранение расходов в российских рублях
 }
+val nds: Double = 20.0
 
 @JvmInline
-value class RusRubAmount(val amount: Int) // это встроенный класс: одно свойство и оно инициализируется в первичном конструкторе
+value class RusRubAmount(val amount: Double) : PrettyPrintable { // это встроенный класс: одно свойство и оно инициализируется в первичном конструкторе
+
+    // fixme: нужна точность до 2-х знаков после запятой
+    val vat get() = Math.round(amount - (amount / (1 + nds/100)))
+
+    override fun prettyPrint() {
+        println("${amount}руб.")
+    }
+}
 
 fun main() {
-    addExpecnce(RusRubAmount(170)) // американо 300 в Шкафу
+    addExpecnce(RusRubAmount(10.0)) // американо 300 в Шкафу
+
+    val expense = RusRubAmount(170.0)
+    println("В том числе НДС: ${expense.vat} руб.")
+    expense.prettyPrint()
 }
---
+++ src/main/kotlin/ch04_classes_interfaces/embeded_classes/PrettyPrintable.kt
@@ -0,0 +1,5 @@
+package ch04_classes_interfaces.embeded_classes
+
+interface PrettyPrintable {
+    fun prettyPrint()
+}
