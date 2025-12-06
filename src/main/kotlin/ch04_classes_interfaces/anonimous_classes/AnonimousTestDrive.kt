55f1de724d9974210a50ec10bea7fff888027ffe
andrey shalagin <andreyshalagin@gmail.com>
2025-12-06 11:01:36 +0300

    Доступ к локальной переменной из анонимного объекта

--- src/main/kotlin/ch04_classes_interfaces/anonimous_classes/AnonimousTestDrive.kt
+++ src/main/kotlin/ch04_classes_interfaces/anonimous_classes/AnonimousTestDrive.kt
@@ -1,13 +1,14 @@
 package ch04_classes_interfaces.anonimous_classes
 
 fun main() {
+    var clickCount = 0
     val listener = object : MouseListener {
         override fun onEnter() {
             TODO("Not yet implemented")
         }
 
         override fun onClick() {
-            TODO("Not yet implemented")
+            clickCount++
         }
     }
 
