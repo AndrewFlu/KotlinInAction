6bb1550c6d895fecaf99ba1a64f4e8cb45043bdd
andrey shalagin <andreyshalagin@gmail.com>
2025-12-07 09:24:38 +0300

    Синтаксис лямбда-выражений: { params -> body }

---
+++ src/main/kotlin/ch_05_lambdas/basics/UtilsBasedOnLambdas.kt
@@ -0,0 +1,11 @@
+package ch_05_lambdas.basics
+
+fun main() {
+    val sum = { x: Double, y: Double -> x + y }
+    val ndfl_18 = { income: Double -> income - (income / 100 * 18) }
+
+    val salary = 100_000.0
+    val passive_income = 10_000.0
+    println("Оклад: ${salary}, после вычета НДФЛ (18%): ${ndfl_18(salary)}")
+    println("Общий доход: ${sum(passive_income, ndfl_18(salary))}")
+}
