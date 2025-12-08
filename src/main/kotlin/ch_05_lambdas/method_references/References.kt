11e1e367e71d5013fa97774935fea1952e59f60e
andrey shalagin <andreyshalagin@gmail.com>
2025-12-08 20:37:29 +0300

    Ссылка на член (функция с двумя аргументами)

--- src/main/kotlin/ch_05_lambdas/method_references/References.kt
+++ src/main/kotlin/ch_05_lambdas/method_references/References.kt
@@ -1,7 +1,22 @@
 package ch_05_lambdas.method_references
 
+import ch04_classes_interfaces.companion.Person
+
 fun salute() = println("Salute!")
+
+fun sendEmail(person: Person, message: String) {
+    println("Message was sent to ${person.name}")
+}
+
+val action = { person: Person, message: String ->
+    sendEmail(person, message)
+}
+
+val nextAction = ::sendEmail // ссылка на член
 
 fun main() {
     run(::salute)
+
+    action(Person("Anna"), "Hi!")
+    nextAction(Person("Nastya"), "Come here")
 }
