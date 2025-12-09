13e5a1e477e61c38146cd6af13052a9047a98e0d
andrey shalagin <andreyshalagin@gmail.com>
2025-12-09 14:25:41 +0300

    Вызов Java-метода, принимающего функциональный интерфейс из Котлина: ФИ передаётся в качестве объектного выражения

--- src/main/kotlin/ch_05_lambdas/functional_interfaces/RunnableTestDrive.kt
+++ src/main/kotlin/ch_05_lambdas/functional_interfaces/RunnableTestDrive.kt
@@ -4,4 +4,9 @@ fun main() {
     println("Before postponeComputation()")
     Computations().postponeComputation(100) { println("This is lambda parameter") }
     println("After postponeComputation()")
+    Computations().postponeComputation(50, object : Runnable {
+        override fun run() {
+            println("Overriden runnable into object expression")
+        }
+    })
 }
