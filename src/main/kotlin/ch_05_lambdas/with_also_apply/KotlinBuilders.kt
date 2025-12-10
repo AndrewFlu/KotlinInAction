22ae6aa2b972a641c16ea0b688a6bd081f05536c
andrey shalagin <andreyshalagin@gmail.com>
2025-12-10 20:00:06 +0300

    Дополнительные действия с объектом с помощью also {}

--- src/main/kotlin/ch_05_lambdas/with_also_apply/KotlinBuilders.kt
+++ src/main/kotlin/ch_05_lambdas/with_also_apply/KotlinBuilders.kt
@@ -18,3 +18,17 @@ val medals = buildMap<String, Int> {
     put("Gold", 1)
     putAll(listOf("Silver" to 2, "Bronze" to 3))
 }
+
+fun main() {
+    val fruits = listOf("Apple", "Banana", "Cherry")
+    val uppercaseFruits = mutableListOf<String>()
+    val reversedLongFruits = fruits
+        .map { it.uppercase() }
+        .also { uppercaseFruits.addAll(it) }
+        .filter { it.length > 5 }
+        .also { println(it) }
+        .reversed()
+    println()
+    println(uppercaseFruits)
+    println(reversedLongFruits)
+}
