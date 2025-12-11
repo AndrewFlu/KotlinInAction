697b52f68effcc3c48d142abe7d5f99c45f6ce86
andrey shalagin <andreyshalagin@gmail.com>
2025-12-11 15:09:05 +0300

    Коллекции: сочетание map и filter

--- src/main/kotlin/ch_06_collections/filter_map/FilterAndMap.kt
+++ src/main/kotlin/ch_06_collections/filter_map/FilterAndMap.kt
@@ -8,4 +8,10 @@ fun main() {
 
     val income = listOf(400, 45, 1300)
     println("После вычета НДФЛ 18%: ${income.map { it * 0.82 }}")
+
+    println("Имена: ${people.map(People::name)}")
+
+    println("Имена совершеннолетних: ${people.filter { it.age >= 18 }.map { it.name }}")
+
+    println("Самый большой возраст в списке: ${people.maxByOrNull(People::age)?.age}")
 }
