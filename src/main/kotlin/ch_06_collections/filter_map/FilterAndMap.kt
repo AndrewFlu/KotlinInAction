a507a91d35d71780230ef0a67c1d3a3df9a08b1b
andrey shalagin <andreyshalagin@gmail.com>
2025-12-11 20:38:47 +0300

    Поиск самого взрослого человека

--- src/main/kotlin/ch_06_collections/filter_map/FilterAndMap.kt
+++ src/main/kotlin/ch_06_collections/filter_map/FilterAndMap.kt
@@ -1,6 +1,7 @@
 package ch_06_collections.filter_map
 
 import ch_05_lambdas.basics.People
+import ch_05_lambdas.method_references.Person
 
 fun main() {
     val people = listOf(People("Alice", 15), People("Robert", 20), People("Mike", 21))
@@ -14,4 +15,17 @@ fun main() {
     println("Имена совершеннолетних: ${people.filter { it.age >= 18 }.map { it.name }}")
 
     println("Самый большой возраст в списке: ${people.maxByOrNull(People::age)?.age}")
+
+    // неоптимальная функция: поиск будет выполняться для КАЖДОГО элемента в коллекции
+    val unoptimizedOldestPerson = people.filter {
+        val oldestPerson = people.maxByOrNull(People::age)
+        it.age == oldestPerson?.age
+    }
+    println("Самый старший человек: ${unoptimizedOldestPerson}")
+
+    // оптимальный код: вычисляет максимальный возраст всего оддин раз
+    val maxAge = people.maxByOrNull(People::age)?.age
+    val oldestPerson = people.filter { it.age == maxAge }
+    println("Самый старший человек: ${oldestPerson}")
+
 }
