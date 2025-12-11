d12c1f019a80d2d94062d30e7c547633e28c560e
andrey shalagin <andreyshalagin@gmail.com>
2025-12-11 15:03:17 +0300

    Коллекции: маппинг лямбды к элементам коллекции

--- src/main/kotlin/ch_06_collections/filter_map/FilterAndMap.kt
+++ src/main/kotlin/ch_06_collections/filter_map/FilterAndMap.kt
@@ -5,4 +5,7 @@ import ch_05_lambdas.basics.People
 fun main() {
     val people = listOf(People("Alice", 15), People("Robert", 20), People("Mike", 21))
     println("Совершеннолетние: ${people.filter { it.age > 18 }}")
+
+    val income = listOf(400, 45, 1300)
+    println("После вычета НДФЛ 18%: ${income.map { it * 0.82 }}")
 }
