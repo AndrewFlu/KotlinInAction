d397706f34afa95d0333713494e727d148a3623c
andrey shalagin <andreyshalagin@gmail.com>
2025-12-12 08:42:12 +0300

    Применение функций аккумуляуции с инициализатором: * fold() * runningFold()

---
+++ src/main/kotlin/reduce_fold/FoldTestDrive.kt
@@ -0,0 +1,17 @@
+package reduce_fold
+
+import ch_05_lambdas.method_references.Person
+
+fun main() {
+    val people = listOf(
+        Person("Mike", 33),
+        Person("Robert", 34),
+        Person("Kim", 24)
+    )
+    val foldedNames = people.fold("people: ") { acc, person ->
+        acc + person.name
+    }
+    val runningFoldedNames = people.runningFold(initial = "", operation = {acc, person -> acc + person.name})
+    println(foldedNames)
+    println(runningFoldedNames)
+}
