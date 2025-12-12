a94e49329c49e882dbbe42ef43936165f0ebd3f2
andrey shalagin <andreyshalagin@gmail.com>
2025-12-12 08:42:12 +0300

    Применение функций аккумуляуции: * reduce() * runningReduce()

---
+++ src/main/kotlin/reduce_fold/Reduce&Fold.kt
@@ -0,0 +1,23 @@
+package reduce_fold
+
+fun main() {
+    val list = listOf(1, 2, 3, 4)
+
+    val summed = list.reduce { acc, element ->
+        acc + element
+    }
+    val runningSummed = list.runningReduce{ acc, element ->
+        acc + element
+    }
+    println("reduce(sum elements): $summed")
+    println("runningReduce(): $runningSummed")
+
+    val multiplied = list.reduce{ acc, element ->
+        acc * element
+    }
+    val runningMultiplied = list.runningReduce{acc, element ->
+        acc * element
+    }
+    println("reduce(multiply elements): $multiplied")
+    println("runningReduce(): $runningMultiplied")
+}
