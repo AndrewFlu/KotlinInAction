b117a31c97f8de71f8ab8f1319e160f43fa737a3
andrey shalagin <andreyshalagin@gmail.com>
2025-12-13 07:26:13 +0300

    Применение функции для получения Map: * associateWith() — лямбда формирует значения * associateBy() — лямбда формирует ключи

--- src/main/kotlin/ch_06_collections/predicates/Associate.kt
+++ src/main/kotlin/ch_06_collections/predicates/Associate.kt
@@ -4,9 +4,19 @@ fun main() {
     val carOwners = listOf(
         CarOwner("Zhenya", "Geely Atlas Pro", 1.5),
         CarOwner("Ilya", "Toyota Camry", 2.0),
-        CarOwner("Juran", "BMW X6", 4.4)
+        CarOwner("Juran", "BMW X6", 4.4),
+        CarOwner("Tsyplenkov", "Geely Atlas Pro", 1.5),
     )
     val carMap = carOwners.associate { carOwner -> carOwner.name to carOwner.auto }
     println(carMap)
     println(carMap["Ilya"])
+
+    println()
+    // параметр извлекает значения. Ключами выступают элементы коллекции
+    val carAsValueMap = carOwners.associateWith(CarOwner::auto)
+    println("associateWith(): $carAsValueMap")
+    println()
+    // лямбд извлекает ключи. Значениями выступают сами элементы коллекции
+    val carAsKeyMap = carOwners.associateBy { it.auto }
+    println("associateBy(): $carAsKeyMap")
 }
