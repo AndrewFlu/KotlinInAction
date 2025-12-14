6287a13d2d668775df525d2a1bd18de42289337f
andrey shalagin <andreyshalagin@gmail.com>
2025-12-14 10:24:07 +0300

    Цепочка вызовов оператора безопасного вызова

---
+++ src/main/kotlin/ch_07_nullable/nullable_types/NullableChain.kt
@@ -0,0 +1,21 @@
+package ch_07_nullable.nullable_types
+
+class Address(val country: String, val city: String, val street: String, val zipCode: Int)
+
+class Company(val name: String, val address: Address?)
+
+class Person(val name: String, val company: Company?)
+
+// функция-расширение
+fun Person.getCountry(): String {
+    val country = this.company?.address?.country // вызов операторов безопасного вызова в цепочке
+    return if (country != null) country else "unknown"
+}
+
+fun main() {
+    val luba = Person("Luba", null)
+    val zhenya = Person("Zhenya", Company("Moneta", Address("Russia", "Yo", "Gogolya", 424000)))
+
+    println("${luba.name} has a country: ${luba.getCountry()}")
+    println("${zhenya.name} has a country: ${zhenya.getCountry()}")
+}
