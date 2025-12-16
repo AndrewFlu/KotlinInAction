82526d0a937c29d40627f21a12b51e27cc931f1b
andrey shalagin <andreyshalagin@gmail.com>
2025-12-16 08:16:27 +0300

    Рефакторинг: объявление переменной, требующей отложенной инициализации с помощью ключевого слова lateInit

--- src/test/kotlin/ch_07_nullable/late_init/MyServiceTest.kt
+++ src/test/kotlin/ch_07_nullable/late_init/MyServiceTest.kt
@@ -8,7 +8,8 @@ import org.junit.jupiter.api.TestInstance
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class MyServiceTest {
 
-    private var myService: MyService? = null // объявление свойства с типом не допускающим null, с допуском null
+    //    private var myService: MyService? = null // объявление свойства с типом не допускающим null, с допуском null
+    private lateinit var myService: MyService // объявление свойства не null-типа без инициализатора
 
     @BeforeAll
     fun setUp() {
@@ -17,6 +18,7 @@ class MyServiceTest {
 
     @Test
     fun testAction() {
-        assertEquals("Action Done!", myService!!.performAction()) // необходимо позаботиться о null-безопасности
+//        assertEquals("Action Done!", myService!!.performAction()) // необходимо позаботиться о null-безопасности
+        assertEquals("Action Done!", myService.performAction()) // обращение к свойству без дополнительных null-проверок
     }
 }
