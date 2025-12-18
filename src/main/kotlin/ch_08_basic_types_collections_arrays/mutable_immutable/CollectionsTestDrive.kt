ad82c2eb6384ca6f67de58e61afca332d1033ee9
andrey shalagin <andreyshalagin@gmail.com>
2025-12-18 08:47:19 +0300

    Использвоание интрефейсов коллекций: * Collection — только для чтения * MutableCollection — для модификации

---
+++ src/main/kotlin/ch_08_basic_types_collections_arrays/mutable_immutable/CollectionsTestDrive.kt
@@ -0,0 +1,13 @@
+package ch_08_basic_types_collections_arrays.mutable_immutable
+
+fun <T> copyElements(source: Collection<T>, target: MutableCollection<T>) {
+    for (element in source)
+        target.add(element)
+}
+
+fun main() {
+    val source: Collection<Int> = arrayListOf(1, 2, 3, 4, 5, 6)
+    val target: MutableCollection<Int> = arrayListOf(7)
+    copyElements(source, target) // модификация target
+    println(target)
+}
