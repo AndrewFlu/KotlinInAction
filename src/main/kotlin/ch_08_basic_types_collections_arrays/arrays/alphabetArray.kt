9263aeb3df6a5e73dcb7c35020eebf3437b76d9c
andrey shalagin <andreyshalagin@gmail.com>
2025-12-19 07:32:56 +0300

    При созданиии массива тип можно не указывать: компилятор сам выводит нужный тип

--- src/main/kotlin/ch_08_basic_types_collections_arrays/arrays/alphabetArray.kt
+++ src/main/kotlin/ch_08_basic_types_collections_arrays/arrays/alphabetArray.kt
@@ -1,6 +1,6 @@
 package ch_08_basic_types_collections_arrays.arrays
 
 fun main() {
-    val letters = Array<String>(26) {index -> ('a' + index).toString()}
+    val letters = Array(26) {index -> ('a' + index).toString()}
     println(letters.joinToString(""))
 }
