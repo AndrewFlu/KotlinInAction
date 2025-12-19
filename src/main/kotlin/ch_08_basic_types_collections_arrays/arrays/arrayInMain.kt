eae0f24ffa0837946683c3ffec41e9fb00b013eb
andrey shalagin <andreyshalagin@gmail.com>
2025-12-19 07:55:15 +0300

    Добавлена обработка параметров метода main() в функциональном стиле: с использованием forEachIndexed()

--- src/main/kotlin/ch_08_basic_types_collections_arrays/arrays/arrayInMain.kt
+++ src/main/kotlin/ch_08_basic_types_collections_arrays/arrays/arrayInMain.kt
@@ -1,6 +1,12 @@
 package ch_08_basic_types_collections_arrays.arrays
 
+// 1. В императивном стиле
+//fun main(args: Array<String>) {
+//    for (element in args.indices)
+//        println("Argument $element is: ${args[element]}")
+//}
+
+// 2. В функциональном стиле
 fun main(args: Array<String>) {
-  for (element in args.indices)
-      println("Argument $element is: ${args[element]}")
+    args.forEachIndexed { index, element -> println("Argument $index is $element") }
 }
