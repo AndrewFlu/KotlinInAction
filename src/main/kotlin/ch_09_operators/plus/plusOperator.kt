93f7ba7410af9e50c91ea434fa0cbe545f111d23
andrey shalagin <andreyshalagin@gmail.com>
2025-12-19 17:39:37 +0300

    Добавлена ещё одна перегрузка оператора: * (times) как функции-рсширения

--- src/main/kotlin/ch_09_operators/plus/plusOperator.kt
+++ src/main/kotlin/ch_09_operators/plus/plusOperator.kt
@@ -2,13 +2,24 @@ package ch_09_operators.plus
 
 data class Point(val x: Int, val y: Int)
 
+// перегрузка оператора +
 operator fun Point.plus(other: Point): Point {
     return Point((x + other.x), (y + other.y))
+}
+
+// перегрузка оператора *
+operator fun Point.times(scale: Int): Point {
+    return Point((x * scale), (y * scale))
 }
 
 fun main() {
     val p1 = Point(10, 20)
     val p2 = Point(30, 40)
+    // проверяем оператор +
     println(p1.plus(p2)) // как вызов функции
     println(p1 + p2) // как вызов оператора +
+
+    // проверяем оператор *
+    println(p1.times(2)) // как вызов функции
+    println(p1 * 2) // как вызов оператора *
 }
