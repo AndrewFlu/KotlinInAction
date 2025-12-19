962bd6ef15a582794dfebcd197d82364572bc123
andrey shalagin <andreyshalagin@gmail.com>
2025-12-19 17:27:51 +0300

    Перегрузка арифметического оператора '+' в классе Point

---
+++ src/main/kotlin/ch_09_operators/plus/plusOperator.kt
@@ -0,0 +1,14 @@
+package ch_09_operators.plus
+
+data class Point(val x: Int, val y: Int) {
+    operator fun plus(other: Point): Point {
+        return Point((x + other.x), (y + other.y))
+    }
+}
+
+fun main() {
+    val p1 = Point(10, 20)
+    val p2 = Point(30, 40)
+    println(p1.plus(p2)) // как вызов функции
+    println(p1 + p2) // как вызов оператора +
+}
