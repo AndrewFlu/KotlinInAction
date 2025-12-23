61f2030b392ea5572a7bddd22f8640e44397d7d3
andrey shalagin <andreyshalagin@gmail.com>
2025-12-23 08:27:34 +0300

    Использование деструктуризации для упаковки.распаковки множества данных, собранных в одном объекте

---
+++ src/main/kotlin/ch_09_operators/destructurization/MultipleDataReturnInOneFunction.kt
@@ -0,0 +1,16 @@
+package ch_09_operators.destructurization
+
+// объявление класса данных для хранения значений
+data class NameComponents(val name: String, val extension: String)
+
+fun splitFileName(fullName: String): NameComponents {
+    val result = fullName.split('.', limit = 2)
+
+    return NameComponents(result[0], result[1]) // возврат экземпляра данных из функции
+}
+
+fun main() {
+    val (name, ext) = splitFileName("example.kt") // использование деструктуризации для распаковки класса
+    println(name)
+    println(ext)
+}
