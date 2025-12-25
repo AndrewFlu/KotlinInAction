c8d5089c99684d546b49835c83fd863f5c572b6c
andrey shalagin <andreyshalagin@gmail.com>
2025-12-25 22:53:21 +0300

    Рефакторинг_3: использование делегированных свойств для обеспечения наблюдаемости свойств

--- src/main/kotlin/ch_09_operators/delegates/observers/Person.kt
+++ src/main/kotlin/ch_09_operators/delegates/observers/Person.kt
@@ -1,17 +1,7 @@
 package ch_09_operators.delegates.observers
 
 class Person(name: String, val age: Int, salary: Int): Observable() {
-    val _name = ObservableProperty("name", name, this)
-    var name: Any
-        get() = _name.getValue()
-        set(newValue) {
-            _name.setValue(newValue)
-        }
+    var name by ObservableProperty(name, this)
 
-    val _salary = ObservableProperty("salary", salary, this)
-    var salary: Any
-        get() = _salary.getValue()
-        set(newValue) {
-            _salary.setValue(newValue)
-        }
+    var salary by ObservableProperty(salary, this)
 }
