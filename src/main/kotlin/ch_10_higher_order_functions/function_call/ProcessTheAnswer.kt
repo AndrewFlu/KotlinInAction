8ae3689c6a18778ebeec94b48d48cd3f1a33cde0
andrey shalagin <andreyshalagin@gmail.com>
2025-12-29 18:41:25 +0300

    Вызов метода, принимающего аргумент, типом которого является функция из Java-кода

---
+++ src/main/kotlin/ch_10_higher_order_functions/function_call/ProcessTheAnswer.kt
@@ -0,0 +1,5 @@
+package ch_10_higher_order_functions.function_call
+
+fun processTheAnswer(f: (Int) -> Int) {
+    println(f(5))
+}
---
+++ src/main/kotlin/ch_10_higher_order_functions/function_call/TestProcessTheAnswer.java
@@ -0,0 +1,7 @@
+package ch_10_higher_order_functions.function_call;
+
+public class TestProcessTheAnswer {
+    public static void main(String[] args) {
+        ProcessTheAnswerKt.processTheAnswer(number -> number + 105);
+    }
+}
