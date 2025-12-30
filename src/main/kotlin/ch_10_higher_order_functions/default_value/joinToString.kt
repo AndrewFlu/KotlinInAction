fe97181d3effa2ad16ea5a5502f1ad513ab7bc14
andrey shalagin <andreyshalagin@gmail.com>
2025-12-30 09:44:13 +0300

    Добавлена функция, принимающая аргумент с типом "функция", который допускает значение null

--- src/main/kotlin/ch_10_higher_order_functions/default_value/joinToString.kt
+++ src/main/kotlin/ch_10_higher_order_functions/default_value/joinToString.kt
@@ -18,9 +18,32 @@ fun <T> Collection<T>.joinToString(
     return result.toString()
 }
 
+fun <T> Collection<T>.nullableJoinToString(
+    separator: String = ", ",
+    prefix: String = "",
+    postfix: String = "",
+    transform: ((T) -> String)? = null // параметр с типом "функция", который допускает null
+): String {
+    val result = StringBuilder(prefix)
+    for ((index, element) in this.withIndex()) {
+        if (index > 0) result.append(separator)
+        val str = (transform?.invoke(element) // синтаксис безопасного вызова функции
+            ?: element.toString()) // оператор '?:' для обработки случая, когда обратный вызов не был указан
+        result.append(str)
+    }
+    result.append(postfix)
+    return result.toString()
+}
+
 fun main() {
     val letters = listOf("alpha", "beta")
     println(letters.joinToString()) // функция преобразования с дефолтными параметрами
     println(letters.joinToString { it.uppercase() }) // передача лямбды в качестве аргумента
-    println(letters.joinToString(separator = " / ", postfix = "! ", transform = { it + " star"})) // использование именованных аргументов
+    println(
+        letters.joinToString(
+            separator = " / ",
+            postfix = "! ",
+            transform = { it + " star" })
+    ) // использование именованных аргументов
+    println(letters.nullableJoinToString(transform = { s -> s + " letter" }))
 }
