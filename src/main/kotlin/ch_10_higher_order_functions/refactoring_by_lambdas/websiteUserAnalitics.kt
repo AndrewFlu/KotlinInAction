33429043bff2f9ef6da144781a34925e6916866f
andrey shalagin <andreyshalagin@gmail.com>
2026-01-01 18:30:23 +0300

    Программа рассчёта сессии клиента. Итерация 1: расчёт средней сессии пользователей Windows

---
+++ src/main/kotlin/ch_10_higher_order_functions/refactoring_by_lambdas/websiteUserAnalitics.kt
@@ -0,0 +1,28 @@
+package ch_10_higher_order_functions.refactoring_by_lambdas
+
+data class SiteVisit(val path: String, val duration: Double, val os: OS)
+
+enum class OS {
+    WINDOWS,
+    MAC_OS,
+    LINUX,
+    IOS,
+    ANDROID,
+}
+
+val log = listOf(
+    SiteVisit("/", 30.0, OS.WINDOWS),
+    SiteVisit("/", 22.0, OS.MAC_OS),
+    SiteVisit("/login", 12.0, OS.WINDOWS),
+    SiteVisit("/signup", 8.0, OS.IOS),
+    SiteVisit("/", 16.3, OS.ANDROID)
+)
+
+fun windowsSessionDurationAverage() =
+    log.filter { it.os == OS.WINDOWS }
+        .map(SiteVisit::duration)
+        .average()
+
+fun main() {
+    println("Средняя продолжительность посещений с машин Windows: ${windowsSessionDurationAverage()}")
+}
