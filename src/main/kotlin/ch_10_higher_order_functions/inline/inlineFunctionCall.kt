986a2f87d9ba7ebfb8a698cd1d493d40e913982b
andrey shalagin <andreyshalagin@gmail.com>
2026-01-02 09:59:17 +0300

    Пример объявления встроенной функции (inline fun()). Такой синтаксис позволяет компилятору встраивать тело такой функции в местах её вызова. Что оптимизирует работу кода

---
+++ src/main/kotlin/ch_10_higher_order_functions/inline/inlineFunctionCall.kt
@@ -0,0 +1,20 @@
+package ch_10_higher_order_functions.inline
+
+import java.util.concurrent.locks.Lock
+import java.util.concurrent.locks.ReentrantLock
+
+
+// тело функции, помеченнрй как inline подставляется в точки вызова функции
+inline fun <T> synchronized(lock: Lock, action: () -> T): T {
+    lock.lock()
+    try {
+        return action()
+    } finally {
+        lock.unlock()
+    }
+}
+
+fun main() {
+    val lock = ReentrantLock()
+    synchronized(lock) { println("Lambda") }
+}
