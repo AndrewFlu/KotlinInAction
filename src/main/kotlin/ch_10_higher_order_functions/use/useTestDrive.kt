5b3bff3576cf971189c67175526722f6cd6ee9c2
andrey shalagin <andreyshalagin@gmail.com>
2026-01-03 11:15:49 +0300

    Применение функции useLines для считывания первой строки в файле

--- src/main/kotlin/ch_10_higher_order_functions/use/useTestDrive.kt
+++ src/main/kotlin/ch_10_higher_order_functions/use/useTestDrive.kt
@@ -1,8 +1,9 @@
 package ch_10_higher_order_functions.use
 
 import java.io.BufferedReader
-import java.io.File
 import java.io.FileReader
+import kotlin.io.path.Path
+import kotlin.io.path.useLines
 
 fun readFirstLine(file: String): String {
     BufferedReader(FileReader(file)).use { br -> // вызов use и передача лямбды для операции с файлом
@@ -10,7 +11,15 @@ fun readFirstLine(file: String): String {
     }
 }
 
+fun readFirstLineByUseLines(file: String): String {
+    Path(file).useLines {
+        return it.first() // it — последовательность строк из входного файла
+    }
+}
+
 fun main() {
     val file = "src/main/resources/ForReadFirstLine.txt"
-    println("First line into file [$file]: ${readFirstLine(file)}")
+    println("First line into file [$file] by use(): ${readFirstLine(file)}")
+
+    println("First line into file [$file] by useLines(): ${readFirstLineByUseLines(file)}")
 }
