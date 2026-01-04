468678e8b0b8b25142ce1995ec6b061e26ca3c27
andrey shalagin <andreyshalagin@gmail.com>
2026-01-04 09:15:04 +0300

    Использование оператора return из лямбды

--- src/main/kotlin/ch_10_higher_order_functions/closures/retuningFromLambdas.kt
+++ src/main/kotlin/ch_10_higher_order_functions/closures/retuningFromLambdas.kt
@@ -7,13 +7,25 @@ fun findZhenya(people: List<Person>) {
     for (person in people) {
         if (person.name == "Zhenya") {
             println("Found!")
-            return
+            return // возврат из функции
         }
     }
     println("Zhenya is not found!")
 }
 
+fun findJuran(people: List<Person>) {
+    people.forEach{
+        if (it.name == "Juran") {
+            println("Juran found!")
+            return // возврат из функции
+        }
+    }
+    println("Juran is not found!")
+
+}
+
 fun main() {
 val people = listOf(Person("Juran", 42), Person("Zhenya", 38))
     findZhenya(people)
+    findJuran(people)
 }
