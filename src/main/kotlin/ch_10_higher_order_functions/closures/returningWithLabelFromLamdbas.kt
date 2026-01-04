6f12f9ab12d3885e36f79baea13b1942efd7a80c
andrey shalagin <andreyshalagin@gmail.com>
2026-01-04 09:29:44 +0300

    Локальный возврат из лямбды с использование метки

---
+++ src/main/kotlin/ch_10_higher_order_functions/closures/returningWithLabelFromLamdbas.kt
@@ -0,0 +1,18 @@
+package ch_10_higher_order_functions.closures
+
+data class Macbook(val model: String, val year: Int)
+
+
+fun findMacbookPro(macbooks: List<Macbook>) {
+    macbooks.forEach macbookLabel@{
+        if (it.model != "MacBook Pro") {
+            return@macbookLabel
+        }
+        println("'MacBook Pro' found!") // эта строка выполнится, если не выполнился возврат return@macbookLabel
+    }
+}
+
+fun main() {
+    val macbooks = listOf(Macbook("MacBook Air", 2017), Macbook("MacBook Pro", 2019))
+    findMacbookPro(macbooks)
+}
