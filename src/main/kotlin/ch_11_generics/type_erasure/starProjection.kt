d054deb3581896d6e1e027944fd3ffa202eb8f2e
andrey shalagin <andreyshalagin@gmail.com>
2026-01-06 10:23:05 +0300

    Приведение типа с обобщённым типом (star projection)

---
+++ src/main/kotlin/ch_11_generics/type_erasure/starProjection.kt
@@ -0,0 +1,11 @@
+package ch_11_generics.type_erasure
+
+fun printSum(collection: Collection<*>) {
+    val intList = collection as? List<Int> ?: throw IllegalArgumentException("List is expected")
+    println(intList.sum())
+}
+
+fun main() {
+    printSum(listOf(1, 2, 3, 4, 5))
+    printSum(setOf(1, 2, 3, 4, 5)) // IllegalArgumentException: List is expected
+}
