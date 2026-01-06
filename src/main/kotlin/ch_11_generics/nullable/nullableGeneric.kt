36d6b60bbc39c4bb9cc773c0903e12d25cd3b755
andrey shalagin <andreyshalagin@gmail.com>
2026-01-06 09:37:42 +0300

    Обобщённый тип с ограничением верхней границы НЕ допускает значение null

--- src/main/kotlin/ch_11_generics/nullable/nullableGeneric.kt
+++ src/main/kotlin/ch_11_generics/nullable/nullableGeneric.kt
@@ -1,8 +1,14 @@
 package ch_11_generics.nullable
 
-class Processor<T> {
+class Processor<T> { // T без ограничений ДОПУСКАЕТ значение null
     fun process(value: T): Int? {
         return value?.hashCode() // value допускает значение null
+    }
+}
+
+class Consumer<T : Any> { // T с ограничением верхней границы НЕ допускает значение null
+    fun accept(element: T) {
+        println("Accepted: $element" )
     }
 }
 
@@ -10,4 +16,7 @@ fun main() {
     val nullableStringProcessor = Processor<String?>()
     println(nullableStringProcessor.process(null))
     println(nullableStringProcessor.process("a"))
+    println()
+    val consumer = Consumer<String>()
+    consumer.accept("1")
 }
