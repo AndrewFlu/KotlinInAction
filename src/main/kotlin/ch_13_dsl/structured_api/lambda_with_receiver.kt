e6b29e387688b7d074bb12520bd45aa5c5b34d76
andrey shalagin <andreyshalagin@gmail.com>
2026-01-18 16:36:48 +0300

    Реализация buildString с аргументом: лямбда с приёмником

--- src/main/kotlin/ch_13_dsl/structured_api/lambda_with_receiver.kt
+++ src/main/kotlin/ch_13_dsl/structured_api/lambda_with_receiver.kt
@@ -1,8 +1,16 @@
 package ch_13_dsl.structured_api
 
+// вариант функции,принимающей лямбду
 fun buildString(builderAction: (StringBuilder) -> Unit): String {
     val sb = StringBuilder()
     builderAction(sb) // передача экземпляра StringBuilder в качестве аргумента в лямбду
+    return sb.toString()
+}
+
+// вариант той же функции, только на основе лямбды с приёмником
+fun buildString2(builderAction: StringBuilder.() -> Unit): String { // объявление праметра типа функции с приёмником
+    val sb = StringBuilder()
+    sb.builderAction() // передача экземпляра StringBuilder в качестве приёмника в лямбду
     return sb.toString()
 }
 
@@ -13,4 +21,10 @@ fun main() {
         it.append("M1 Pro")
     }
     println(s)
+
+    val x = buildString2 {
+        this.append("Windows ") // можно сослаться на this — указатель на экземпляр StringBuilder
+        append("10") // а можно не указывать this и ссылаться на StringBuilder
+    }
+    println(x)
 }
