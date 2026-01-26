5b99113edab2ec64bf7e6556023823700a3f9dfc
andrey shalagin <andreyshalagin@gmail.com>
2026-01-26 09:34:44 +0300

    Применение функции yield() позволяет передавать управление другой корутине. В результате выполнение 2-х корутин launch выполняется поочерёдно: одна за другой, передавая управление друг другу

--- src/main/kotlin/ch_15_structured_concyrrency/yield/yieldTestDrive.kt
+++ src/main/kotlin/ch_15_structured_concyrrency/yield/yieldTestDrive.kt
@@ -3,13 +3,15 @@ package ch_15_structured_concyrrency.yield
 import ch_14_coroutines.coroutines_constructors.log
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.yield
 
-fun doCpuHeavyWork(): Int {
+suspend fun doCpuHeavyWork(): Int {
     log("Cpu working!")
     var counter = 0
     val startTime = System.currentTimeMillis()
     while (System.currentTimeMillis() < startTime + 500) {
         counter++
+        yield()
     }
     return counter
 }
