fb51976c7d8438d76597c5c8f1d2f912515d82b3
andrey shalagin <andreyshalagin@gmail.com>
2026-01-27 21:16:40 +0300

    Запуск коллектора для потока, производящего буквы

--- src/main/kotlin/ch_16_threads/cold_threads/flowConstructor.kt
+++ src/main/kotlin/ch_16_threads/cold_threads/flowConstructor.kt
@@ -1,17 +1,24 @@
 package ch_16_threads.cold_threads
 
-import ch_10_higher_order_functions.refactoring_by_lambdas.log
 import ch_14_coroutines.coroutines_constructors.log
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.runBlocking
 import kotlin.time.Duration.Companion.milliseconds
 
+val letters = flow {
+    log("Emitting A")
+    emit("A")
+    delay(250.milliseconds)
+    log("Emitting B")
+    emit("B")
+}
+
 fun main() {
-    val letters = flow {
-        log("Emitting A")
-        emit("A")
-        delay(250.milliseconds)
-        log("Emitting B")
-        emit("B")
+    runBlocking {
+        letters.collect {
+            log("Collecting $it")
+            delay(500.milliseconds)
+        }
     }
 }
