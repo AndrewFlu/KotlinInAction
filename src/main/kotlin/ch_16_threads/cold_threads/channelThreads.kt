9a9c7de06c092ef3e765e47c29cb63bfd2b86fa6
andrey shalagin <andreyshalagin@gmail.com>
2026-01-28 09:03:33 +0300

    Добавление случайных чисел в канальный поток в конкурентном режиме

--- src/main/kotlin/ch_16_threads/cold_threads/channelThreads.kt
+++ src/main/kotlin/ch_16_threads/cold_threads/channelThreads.kt
@@ -1,7 +1,9 @@
 package ch_16_threads.cold_threads
 
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.channelFlow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import kotlin.random.Random
 import kotlin.time.Duration.Companion.milliseconds
@@ -11,9 +13,19 @@ suspend fun getRandomNumber(): Int {
     return Random.nextInt()
 }
 
-val randomNumbers = flow {
+// последовательная отправка элементов в поток
+//val randomNumbers = flow {
+//    repeat(10) {
+//        emit(getRandomNumber())
+//    }
+//}
+
+// конкурентная отправка элементов в канальный поток
+val randomNumbers = channelFlow { // создание нового канального потока
     repeat(10) {
-        emit(getRandomNumber())
+        launch { // запускаем 10 разных корутин
+            send(getRandomNumber()) // в каждой корутине предлагаем канальному потоку произведённое случайное число
+        }
     }
 }
 
