07e4e0a74702d035793edc98ee14cd8c6c4accc0
andrey shalagin <andreyshalagin@gmail.com>
2026-01-29 08:48:36 +0300

    Пример горячего потока на основе SharedFlow. Значения "выбрасываются" в поток независимо от наличия или отсутствия подпискчиков потока

---
+++ src/main/kotlin/ch_16_threads/hot_threads/sharedFlows.kt
@@ -0,0 +1,36 @@
+package ch_16_threads.hot_threads
+
+import ch_14_coroutines.coroutines_constructors.log
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlin.random.Random
+import kotlin.time.Duration.Companion.milliseconds
+
+class RadioStation {
+    // определение нового изменяемого потока в качестве приватного свойства
+    private val _messageFlow = MutableSharedFlow<Int>()
+
+    // предоставление общего потока в виде, доступном только для чтения
+    val messageFlow = _messageFlow.asSharedFlow()
+
+    fun beginBroadCasting(scope: CoroutineScope) {
+        scope.launch {
+            while (true) {
+                delay(500.milliseconds)
+                val number = Random.nextInt(0 ,10)
+                log("Emiting $number")
+                _messageFlow.emit(number) // выброс значения в изменяемый общий поток из корутины
+            }
+        }
+    }
+}
+
+fun main() {
+    runBlocking {
+        RadioStation().beginBroadCasting(this)
+    }
+}
