2f3d8c314991ce3153f4d419b14a0c996b3ecb13
andrey shalagin <andreyshalagin@gmail.com>
2026-02-02 16:52:12 +0300

    Контекст supervisorScope{} позволяет обернуть вызываемые корутины в контекст супервизора. Это позволит не отменять ни родительскую, ни дочерние корутины при возникновении исключения в одной из дочерних корутин

---
+++ src/main/kotlin/error_handling_and_testing/error_handling/cancel_coroutine_policy/by_supervisor_job/exceptionHandlingViaSupervisor.kt
@@ -0,0 +1,34 @@
+package error_handling_and_testing.error_handling.cancel_coroutine_policy.by_supervisor_job
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.supervisorScope
+import kotlin.time.Duration.Companion.milliseconds
+import kotlin.time.Duration.Companion.seconds
+
+fun main() {
+    runBlocking {
+        // declare supervisor scope
+        supervisorScope {
+            // coroutine 1
+            launch {
+                try {
+                    while (true) {
+                        println("Transmitting data!")
+                        delay(250.milliseconds)
+                    }
+                } catch (ex: Exception) {
+                    println("Transmitting date interrupted: $ex")
+                    throw ex
+                }
+            }
+
+            // coroutine 2
+            launch {
+                delay(1.seconds)
+                throw UnsupportedOperationException("Ouch!")
+            }
+        }
+    }
+}
