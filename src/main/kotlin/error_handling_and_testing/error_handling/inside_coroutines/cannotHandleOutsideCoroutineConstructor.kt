352d140947edba07fd8218ac4f843bf96187882d
andrey shalagin <andreyshalagin@gmail.com>
2026-02-02 08:55:36 +0300

    Исключения, выброшенные из-за пределов конструктора корутины не перехватываются блоком try-catch

---
+++ src/main/kotlin/error_handling_and_testing/error_handling/inside_coroutines/cannotHandleOutsideCoroutineConstructor.kt
@@ -0,0 +1,17 @@
+package error_handling_and_testing.error_handling.inside_coroutines
+
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+fun main() {
+    runBlocking {
+        try {
+            // нельзя перехватить исключение, выброшенное ИЗ КОНСТРУКТОРА корутины
+            launch {
+                throw UnsupportedOperationException("Ouch!")
+            }
+        } catch (e: UnsupportedOperationException) {
+            println("Handled: $e")
+        }
+    }
+}
