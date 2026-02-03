72344714f8bf3a9a424b4a59e611bf41eff47ea2
andrey shalagin <andreyshalagin@gmail.com>
2026-02-03 08:57:47 +0300

    Обработка ошибок в потоках: сбор потока (collect()) оборачивается в блок try-catch

---
+++ src/main/kotlin/error_handling_and_testing/error_handling/in_threads/exceptionalThread.kt
@@ -0,0 +1,30 @@
+package error_handling_and_testing.error_handling.in_threads
+
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.runBlocking
+
+class MacBookException: Exception()
+
+val exceptionalFlow = flow {
+    repeat(5) {
+        model -> emit(model)
+    }
+    throw MacBookException()
+}
+
+fun main() {
+    runBlocking {
+        val transformedFlow = exceptionalFlow.map {
+            it * 2
+        }
+
+        try {
+            transformedFlow.collect {
+                println("$it ")
+            }
+        } catch (me: MacBookException) {
+            println("[HANDLED] ${me.message ?: "Unsupported model"}")
+        }
+    }
+}
