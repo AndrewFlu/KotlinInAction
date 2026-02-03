bd5f2f6d3d625ded737cdbb8efec3350e1050bc5
andrey shalagin <andreyshalagin@gmail.com>
2026-02-03 09:10:43 +0300

    Оператор catch() может перехватывать исключения, выбрасываемые в восходящем потоке. Всё, что после (в нисходящем потоке) — оператор не перехватывает.

---
+++ src/main/kotlin/error_handling_and_testing/error_handling/in_threads/catch_operator/catchHandleOnlyInUpperThreads.kt
@@ -0,0 +1,17 @@
+package error_handling_and_testing.error_handling.in_threads.catch_operator
+
+import error_handling_and_testing.error_handling.in_threads.exceptionalFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.runBlocking
+
+fun main() {
+    runBlocking {
+        exceptionalFlow
+            .map { it + 1 }
+            .catch { cause -> println("Handled: $cause") }
+            .onEach { throw RuntimeException("Runtime Exception") }
+            .collect({})
+    }
+}
