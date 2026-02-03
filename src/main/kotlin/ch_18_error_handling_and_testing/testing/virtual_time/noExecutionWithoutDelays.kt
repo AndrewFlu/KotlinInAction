9c65602724feac21ed947ace6a1a16ad6318a178
andrey shalagin <andreyshalagin@gmail.com>
2026-02-03 14:47:14 +0300

    Если в корутины добавить функции приостановки, то код будет успешно выполняться

--- src/main/kotlin/ch_18_error_handling_and_testing/testing/virtual_time/noExecutionWithoutDelays.kt
+++ src/main/kotlin/ch_18_error_handling_and_testing/testing/virtual_time/noExecutionWithoutDelays.kt
@@ -1,9 +1,13 @@
 package ch_18_error_handling_and_testing.testing.virtual_time
 
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.currentTime
 import kotlinx.coroutines.test.runTest
 import org.junit.Test
 import kotlin.test.assertEquals
+import kotlin.time.Duration.Companion.milliseconds
+import kotlin.time.Duration.Companion.seconds
 
 class DelayTest {
     @Test
@@ -17,5 +21,30 @@ class DelayTest {
         }
 
         assertEquals(2, x)
+    }
+
+    @Test
+    fun testDelay() {
+        runTest {
+            var x = 0
+            launch {
+                delay(500.milliseconds)
+                x++
+            }
+            launch {
+                delay(1.seconds)
+                x++
+            }
+            println(currentTime) // 0
+
+            delay(600.milliseconds)
+            assertEquals(1, x) // т.к. успеет отработать первый инкремент
+            println(currentTime) // 600
+
+            delay(500.milliseconds)
+            assertEquals(2, x) // т.к. уже отработают оба инкремента
+            println(currentTime) // 1100
+        }
+
     }
 }
