b88e77d4988a7533003293e27ec98f866071dd0b
andrey shalagin <andreyshalagin@gmail.com>
2026-02-03 09:05:33 +0300

    Перехват исключения в операторе потока catch() и выброс в этом случае значения по умолчанию

---
+++ src/main/kotlin/error_handling_and_testing/error_handling/in_threads/catch_operator/defaultValueByCatch.kt
@@ -0,0 +1,18 @@
+package error_handling_and_testing.error_handling.in_threads.catch_operator
+
+import error_handling_and_testing.error_handling.in_threads.exceptionalFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.runBlocking
+
+fun main() {
+    runBlocking {
+        exceptionalFlow
+            .catch { cause ->
+                println("Handled: $cause")
+                emit(-1) // значение по умолчанию в случае перехвата исключения
+            }
+            .collect {
+                println("$it ")
+            }
+    }
+}
